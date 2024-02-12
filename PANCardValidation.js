31bd95eee7c73091187086e0c29036d9becb2505
Suresh-Palaparthi <suresh.palaparthi.work@gmail.com>
2024-02-12 15:48:45 +0530

    final changes

--- PANCardValidation.js
+++ PANCardValidation.js
@@ -1,13 +1,24 @@
-function validatePAN(){
+function validatePAN(event){
 
+    event.preventDefault();
     let panNumber = document.getElementById('pan-number').value;
     let panFormat = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
+    let correctValidation = "Your PAN will be delivered to your address.";
+    let incorrectValidation ="Please enter a valid pan number. Recheck the format."
+
+    let validationMessageElement = document.getElementsByTagName('p')[0];
 
     if (panFormat.test(panNumber)){
-        alert("Correct");
+        // alert("Correct");
+        document.getElementsByTagName('p')[0].style.color = '';
+        validationMessageElement.textContent = correctValidation;
     }
 
     else{
-        alert('Incorrect PAN Number format');
+        // alert('Incorrect PAN Number format');
+        document.getElementsByTagName('p')[0].style.color = 'red';
+        validationMessageElement.textContent = incorrectValidation;
     }
 }
+
+document.getElementById('pan-form').addEventListener('submit', validatePAN);
