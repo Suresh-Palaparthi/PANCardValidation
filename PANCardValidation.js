25225f5db869978c105b7555bb23abb60a1b09f1
Suresh-Palaparthi <suresh.palaparthi.work@gmail.com>
2024-02-13 23:23:05 +0530

    Made simple indentation changes

--- PANCardValidation.js
+++ PANCardValidation.js
@@ -1,20 +1,20 @@
-function validatePAN(event){
+function validatePAN(event) {
 
     event.preventDefault();
     let panNumber = document.getElementById('pan-number').value;
     let panFormat = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
     let correctValidation = "Your PAN will be delivered to your address.";
-    let incorrectValidation ="Please enter a valid pan number. Recheck the format."
+    let incorrectValidation = "Please enter a valid pan number. Recheck the format."
 
     let validationMessageElement = document.getElementsByTagName('p')[0];
 
-    if (panFormat.test(panNumber)){
+    if (panFormat.test(panNumber)) {
         // alert("Correct");
         document.getElementsByTagName('p')[0].style.color = '';
         validationMessageElement.textContent = correctValidation;
     }
 
-    else{
+    else {
         // alert('Incorrect PAN Number format');
         document.getElementsByTagName('p')[0].style.color = 'red';
         validationMessageElement.textContent = incorrectValidation;
